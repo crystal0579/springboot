efdfb3efcb6c72f3cac0f4677dc12f5da8927166
sonia <wengjuan025@163.com>
2020-03-24 14:16:37 +0800

    remember me

--- spring_security_management/src/main/java/com/itheima/controller/UserController.java
+++ spring_security_management/src/main/java/com/itheima/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itheima.controller;
 import com.itheima.domain.SysUser;
 import com.itheima.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +45,9 @@ public class UserController {
 
     @RequestMapping("/addRoleToUser")
     public String addRoleToUser(Integer[] ids, Integer userId){
+        //后台拿到登录的用户名
+//        SecurityContextHolder.getContext().getAuthentication().getName();
+
         userService.addRoleToUser(userId, ids);
         return "redirect:toAddRolePage?success="+true+"&id="+userId;
     }
